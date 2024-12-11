283bf3558079e84e4470c504266b7910a41172d0
shafran <shafran.andriy@gmail.com>
2024-12-11 23:01:32 +0200

    Created function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    fil = input("Enter name of the file: ")
+    while fil == "":
+        fil = input("Enter name of the file: ")
+    with open(f"{fil}.txt", "w+") as fin:
+        pass
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        with open(f"{fil}.txt", "a") as final:
+            final.write(f"{content}\n")
+    print(f'File name: "{fil}.txt"\n')
+    with open(f"{fil}.txt", "r+") as fin:
+        fin_list = fin.readlines()
+    for cont in fin_list:
+        print(cont, end="")
 
 
 if __name__ == "__main__":
